8d408224e9e75b0e9ccb85f983b5dbcedd826d79
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-05-21 12:09:40 +0700

    setup layout

---
+++ components/layout.js
@@ -0,0 +1,16 @@
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"]
+});
+
+export default function Layout({ children }) {
+  return (
+      <>
+        <main className={poppins.className + " pb-14"}>
+          {children}
+        </main>
+      </>
+  )
+}
