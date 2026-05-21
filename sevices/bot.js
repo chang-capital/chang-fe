2713a7db5abeaf5994769fa1b171ae4442815483
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-05-21 21:59:26 +0700

    api(stopRsiStrategy&getRsiStrategy): setup

--- sevices/bot.js
+++ sevices/bot.js
@@ -3,10 +3,27 @@ import callApi from "@/config/api";
 
 const ROOT_API = "http://localhost:8000";
 
+// rsi strategy
 export async function startRsiStrategy() {
     const url = `${ROOT_API}/bot/start-rsi`;
     return callApi({
         url,
         method: "POST"
+    })
+}
+
+export async function stopRsiStrategy() {
+    const url = `${ROOT_API}/bot/stop-rsi`;
+    return callApi({
+        url,
+        method: "POST"
+    })
+}
+
+export async function getRsiStatus() {
+    const url = `${ROOT_API}/bot/rsi-status`;
+    return callApi({
+        url,
+        method: "GET"
     })
 }
