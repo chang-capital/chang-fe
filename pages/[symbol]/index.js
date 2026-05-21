b0c225126e46a770f877cc93d54af76e34c41a91
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-05-21 16:29:47 +0700

    setup

--- pages/[symbol]/index.js
+++ pages/[symbol]/index.js
@@ -1,7 +1,13 @@
+import { useRouter } from "next/router"
+
 const Symbol = () => {
+    const router = useRouter()
+    const { symbol } = router.query
+
     return (
-        <div>
-            <h1>Symbol</h1>
+        <div className="mt-24 px-32">
+            <h1 className="text-3xl font-bold">{symbol?.toUpperCase()}</h1>
+            <hr className="mt-4" />
         </div>
     )
 }
