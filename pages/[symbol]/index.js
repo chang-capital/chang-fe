60924476472d8b8ae125399a5366411b857ebe32
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-05-22 21:51:23 +0700

    setup chart 24

--- pages/[symbol]/index.js
+++ pages/[symbol]/index.js
@@ -12,6 +12,13 @@ const Symbol = () => {
             <h1 className="text-3xl font-bold mt-3">{coin}</h1>
             <hr className="mt-4 pb-4" />
             <Link className="text-blue-500" href={`/${coin}/market-flow`}>Market Flow</Link>
+
+            <div className="p-4">
+                <div className="flex justify-between items-center mb-4">
+                    <h2 className="font-semibold text-lg">Price</h2>
+                    <p className="text-xl font-mono font-semibold">$123.45</p>
+                </div>
+            </div>
         </div>
     )
 }
