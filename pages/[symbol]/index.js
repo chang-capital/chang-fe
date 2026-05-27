1e8edd2d0ee5d62d592cdae979641e9131e38932
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-05-27 22:04:52 +0700

    style(button): startBotRSI

--- pages/[symbol]/index.js
+++ pages/[symbol]/index.js
@@ -15,6 +15,24 @@ const Symbol = () => {
             <Link className="text-blue-500" href={`/${coin}/market-flow`}>Market Flow</Link>
 
             <CandlePrice symbol={coin} />
+            <hr className="mt-4" />
+            <div className="py-4 flex justify-between items-center">
+                <h2 className="text-xl font-semibold">
+                    BALANCE
+                </h2>
+                <div className="flex gap-1 flex-col items-end">
+                    <h2 className="text-lg font-semibold">$0.00 <span className="text-[#26A17B] text-sm">USDT</span></h2>
+                    <h2 className="text-lg font-semibold">0.001 <span className="text-sm">
+                        {coin === "BTCUSDT" ? "BTC" : coin === "ETHUSDT" ? "ETH" : coin}
+                    </span></h2>
+                </div>
+            </div>
+            <hr/>
+            <div className="mt-4">
+                <button className="cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded">
+                    Start RSI BOT
+                </button>
+            </div>
         </div>
     )
 }
