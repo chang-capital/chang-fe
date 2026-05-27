102d0f66b4d0aa4e1d25c618f752512e668fc6ee
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-05-27 22:38:13 +0700

    api(getAccount): get balance

--- pages/[symbol]/index.js
+++ pages/[symbol]/index.js
@@ -1,12 +1,41 @@
+import { useEffect, useState } from "react"
 import { useRouter } from "next/router"
 import Link from "next/link"
 import CandlePrice from "@/components/CandlePrice"
+import { getAccountInfo } from "@/services/bot"
 
 const Symbol = () => {
+    const [account, setAccount] = useState(null)
+    const [coinBalance, setCoinBalance] = useState(0)
+    const [usdtBalance, setUsdtBalance] = useState(0)
+
     const router = useRouter()
     const { symbol } = router.query
     const coin = symbol?.toUpperCase()
+    const coinSymbol = coin?.replace(/USDT$/i, '')
 
+    const getAccount = async () => {
+        const data = await getAccountInfo()
+        if(data) {
+            const accountData = data.data.data
+            setAccount(accountData)
+        } else {
+            setAccount(null)
+        }
+    }
+
+    useEffect(() => {
+        getAccount()
+    }, [])
+
+    useEffect(() => {
+        if(account && coinSymbol) {
+            const balance = account.balances.find(b => b.asset === coinSymbol)
+            const usdt = account.balances.find(b => b.asset === 'USDT')
+            setUsdtBalance(usdt ? usdt.free : '0')
+            setCoinBalance(balance ? balance.free : '0')
+        }
+    }, [account, coinSymbol])
     return (
         <div className="mt-24 px-32">
             <Link className="text-blue-500" href="/">Back</Link>
@@ -21,9 +50,9 @@ const Symbol = () => {
                     BALANCE
                 </h2>
                 <div className="flex gap-1 flex-col items-end">
-                    <h2 className="text-lg font-semibold">$0.00 <span className="text-[#26A17B] text-sm">USDT</span></h2>
-                    <h2 className="text-lg font-semibold">0.001 <span className="text-sm">
-                        {coin === "BTCUSDT" ? "BTC" : coin === "ETHUSDT" ? "ETH" : coin}
+                    <h2 className="text-lg font-semibold">${parseFloat(usdtBalance).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })} <span className="text-[#26A17B] text-sm">USDT</span></h2>
+                    <h2 className="text-lg font-semibold">{coinBalance} <span className="text-sm">
+                        {coinSymbol}
                     </span></h2>
                 </div>
             </div>
